237deda70f7e61b752af79398b85ccd9fec0003a
Ali <cafarovali2004@gmail.com>
2024-03-16 23:14:34 +0400

    Updated Tasks

---
+++ Task1/Task1/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Enter the number : ");
+int number = Convert.ToInt32(Console.ReadLine());
+int product = 1;
+
+while (number > 0)
+{
+    var digit = number % 10;
+    number = number / 10;
+
+    if (digit % 5 ==0)
+    {
+        product *= digit;
+    }
+}
+Console.WriteLine("Product of digits divisible by 5: " + product);
---
+++ Task2/Task2/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter the number : ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (number != 0)
+{
+    int digit = number % 10;
+    sum += digit;
+    number /= 10;
+}
+Console.WriteLine("Sum : " + sum);
+
+if (sum % 2 == 0)
+{
+    Console.WriteLine("Sum is even");
+}
+else
+{
+    Console.WriteLine("Sum is odd");
+}
