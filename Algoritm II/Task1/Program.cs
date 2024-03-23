40cbe4fc367e76dfd17f4468c61826d66f048153
Ali <cafarovali2004@gmail.com>
2024-03-23 12:51:38 +0400

    Updated Tasks 2

---
+++ Algoritm II/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿while (true)
+{
+    Console.Write("Enter the number : ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int sum = 0;
+
+    while (number > 0)
+    {
+        var digit = number % 10;
+        number = number / 10;
+
+        if (digit % 2 == 0)
+        {
+            sum += digit;
+        }
+    }
+    if (sum > 0)
+    {
+        if (sum % 4 == 0 && sum % 3 == 0)
+            Console.WriteLine(true);
+        else
+            Console.WriteLine(false);
+    }
+    else
+        Console.WriteLine("Sum is 0.");
+}
---
+++ Algoritm II/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿while (true)
+{
+    Console.Write("Enter the number : ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int newNumber = 0, multiplier = 1;
+
+    while (number > 0)
+    {
+        int digit = number % 10;
+        number /= 10;
+        if (digit % 2 != 0)
+        {
+            newNumber += digit * multiplier;
+            multiplier *= 10;
+        }
+    }
+    Console.WriteLine("The new number : " + newNumber);
+}
