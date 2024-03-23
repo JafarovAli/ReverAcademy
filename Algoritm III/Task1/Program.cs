e2dc9c4f9d7ce4d9e6282c633af5c188f7434046
Ali <cafarovali2004@gmail.com>
2024-03-23 15:31:04 +0400

    Updated Algoritm III

---
+++ Algoritm III/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿while (true)
+{
+    Console.Write("Enter the number : ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int copyNumber = number;
+    int count = 0;
+    while (number > 0 && number !=1 && number%2==0)
+    {
+        number /= 2;
+    }
+    if (number == 1)
+    {
+        while (copyNumber > 0 && copyNumber % 2 == 0)
+        {
+            copyNumber /= 2;
+            count++;
+        }
+        Console.WriteLine("2^" + count);
+    }
+    else
+        Console.WriteLine("The number is not a power of 2");
+}
---
+++ Algoritm III/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿int number1 = 0, number2 = 1, number3, number;
+int index = 1;
+Console.Write("Enter the number of elements: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+for (var i = 2; i < number; ++i)  
+{
+    number3 = number1 + number2;
+    number1 = number2;
+    number2 = number3;
+    index++;
+}
+if (index != -1)
+{
+    Console.WriteLine($"{number} is located at index {index} in the Fibonacci sequence.");
+}
+else
+{
+    Console.WriteLine($"{number} is not part of the Fibonacci sequence.");
+}
