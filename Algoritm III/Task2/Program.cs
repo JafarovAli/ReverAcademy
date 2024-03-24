5ed5894cf77105cb78537a13771197010e4c166e
Ali <cafarovali2004@gmail.com>
2024-03-24 14:29:28 +0400

    Updated Algoritm III Task 2

--- Algoritm III/Task2/Program.cs
+++ Algoritm III/Task2/Program.cs
@@ -1,20 +1,34 @@
-﻿int number1 = 0, number2 = 1, number3, number;
-int index = 1;
-Console.Write("Enter the number of elements: ");
-number = Convert.ToInt32(Console.ReadLine());
+﻿using System;
 
-for (var i = 2; i < number; ++i)  
+int number1 = 0, number2 = 1, number3 = 0, count,i=2;
+Console.Write("Enter the number of elements: ");
+count = Convert.ToInt32(Console.ReadLine());
+if (count == 0)
 {
-    number3 = number1 + number2;
-    number1 = number2;
-    number2 = number3;
-    index++;
+    Console.WriteLine(1);
 }
-if (index != -1)
-{
-    Console.WriteLine($"{number} is located at index {index} in the Fibonacci sequence.");
+else if (count == 1) 
+{  
+    Console.WriteLine(2);
 }
 else
 {
-    Console.WriteLine($"{number} is not part of the Fibonacci sequence.");
+    while (true)
+    {
+        number3 = number1 + number2;
+        number1 = number2;
+        number2 = number3;
+        i++;
+        if (number3 == count)
+        {
+            Console.WriteLine($"{count} is located at index {i} in the Fibonacci sequence.");
+            break;
+        }
+        else if (number3 > count)
+        {
+            Console.WriteLine($"{count} is not part of the Fibonacci sequence.");
+            break;
+        }
+    }
 }
+
