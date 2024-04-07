fa469db4c6c5a2fdadf8fba2269559dad813fcfe
Ali <cafarovali2004@gmail.com>
2024-04-07 10:46:33 +0400

    Updated OOP I

---
+++ OOP I/Task1/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Car
+    {
+        public int serialNumber;
+        public string name;
+        public string brand;
+        public decimal oilLiter;
+        public string color;
+        public bool isAuto;
+        public int year;
+
+        public bool Driving()
+        {
+            return oilLiter > 0;
+        }
+        public decimal GetOilLiter()
+        {
+            return oilLiter;
+        }
+
+        public void GetAllInformation()
+        {
+            Console.WriteLine("Information about the car!");
+            Console.WriteLine($" Serial number : {serialNumber}\n Name : {name}\n Brand: {brand}\n Oil : {oilLiter} litr\n" +
+                              $" Color : {color}\n isAuto : {isAuto}\n Year : {year}\n");
+        }
+    }
+}
---
+++ OOP I/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the oil of car : ");
+            Car car = new()
+            {
+                serialNumber = 1,
+                name = "Bmw",
+                brand = "M5",
+                oilLiter = Convert.ToInt32(Console.ReadLine()),
+                color = "Black",
+                isAuto = true,
+                year = 2024
+            };
+            
+            
+            car.GetAllInformation();
+            Console.WriteLine($"Oil : {car.GetOilLiter()} litr");
+            Console.WriteLine("Can be driven : "+ car.Driving());
+        }
+    }
+}
