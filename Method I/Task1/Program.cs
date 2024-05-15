a989bc3557913c581a2d56010ef84b83db293c00
Ali <cafarovali2004@gmail.com>
2024-05-15 21:50:52 +0400

    Updated SQL I

--- Method I/Task1/Program.cs
+++ Method I/Task1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Task1
 {
@@ -6,13 +8,57 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+
+
             Console.Write("Enter the text: ");
-            string str = Console.ReadLine();
+            string s = Console.ReadLine();
+            Console.WriteLine(LengthOfLongestSubstring(s));
 
-            removeSpaces(ref str);
+            //removeSpaces(ref str);
 
-            Console.WriteLine(str);
+            //Console.WriteLine(str);
         }
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode result = new ListNode(0);
+            ListNode ptr = result;
+            int carry = 0 ;
+            while (l1 != null || l2 != null)
+            {
+                int sum = 0 + carry;
+                if (l1!=null)
+                {
+                    sum += l1.val;
+                    l1 = l1.next;
+                }
+                if (l2 != null)
+                {
+                    sum += l2.val;
+                    l2 = l2.next;
+                }
+                carry = sum / 10;
+                sum = sum % 10;
+                ptr.next = new ListNode(sum);
+                ptr = ptr.next;
+            }
+            if (carry==1)
+            {
+                ptr.next = new ListNode(1);
+            }
+            return result.next;
+        }
+
 
         public static void removeSpaces(ref string str)
         {
@@ -53,6 +99,26 @@ namespace Task1
                 str = str.Remove(i, n - i);
             else
                 str = str.Remove(i - 1, n - i + 1);
+        }
+        public static int LengthOfLongestSubstring(string s)
+        {
+            int count = 0;
+
+            Dictionary<char, int> charIndexMap = new Dictionary<char, int>();
+
+            for (int end = 0, start = 0; end < s.Length; end++)
+            {
+                if (charIndexMap.ContainsKey(s[end]))
+                {
+                    start = Math.Max(charIndexMap[s[end]] + 1, start);
+                }
+
+                count = Math.Max(count, end - start + 1);
+                charIndexMap[s[end]] = end;
+            }
+
+
+            return count;
         }
     }
 }
---
+++ SQL/TASK1.sql
@@ -0,0 +1,40 @@
+﻿/*Create table Products (
+	Id int Primary Key Identity(1,1),
+	Name nvarchar(20),
+	Category nvarchar(20),
+	Price float
+)
+Create table Orders (
+	Id int Primary Key Identity(1,1),
+	Customer_Id int,
+	Product_Id int FOREIGN KEY REFERENCES Products(Id),
+	Order_Date date,
+	Quantity int
+)*/
+
+-- 1.  Müəyyən bir müştərinin (məsələn, customer_id = 1) sifariş etdiyi məhsulların adlarını tapın:
+Select Name from Products
+
+-- 2. Bir kateqoriyada (məsələn, "Electronics") olan bütün məhsulların ümumi sifariş miqdarını tapın:
+Select Quantity from Orders
+
+--3. Bütün məhsulların maksimum sifariş miqdarını tapın:
+Select MAX(Quantity) as MaximumQuantity from Orders
+
+--4. Sifarişlərdə ən çox təkrar edilən məhsul ID-sini tapın:
+SELECT TOP 1 Product_Id, COUNT(*) AS TotalOrders
+FROM Orders
+GROUP BY Product_Id
+ORDER BY TotalOrders Desc
+
+--5. Bütün məhsulların orta qiymətini tapın:
+Select AVG(Price) AS AveragePrice from Products
+
+--6. Ən bahalı məhsulun adını və qiymətini tapın:
+Select Top 1 Name,Price from Products
+Order by Price DESC
+
+--7. Məhsulun (məsələn, product_id = 1) orta sifariş miqdarını tapın:
+
+Select AVG(Quantity) as AverageQuantity from Orders
+Where Product_Id = 1
